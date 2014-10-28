0b38b578ed96d1ae5856a7d26873634daae3d024
Imposimon <imposimon@gmail.com>
2014-10-28 13:46:43 +0800

    add Longest Common Prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public String longestCommonPrefix(String[] strs) {
+		if(strs == null || strs.length == 0) return "";
+		String prefix = strs[0];
+		for(int i = 1; i < strs.length; i++) {
+			int j = 0;
+			while(j < prefix.length() && j < strs[i].length() && prefix.charAt(j) == strs[i].charAt(j)) {
+				j++;
+			}
+			prefix = strs[i].substring(0, j);
+		}
+		return prefix;
+	}
+}
